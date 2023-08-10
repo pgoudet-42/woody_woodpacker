8505e7970336094a998335418d3ac76442685c5c
pgoudet-42 <pgoudet@paul-f4Br8s2.clusters.42paris.fr>
2023-08-10 15:19:33 +0200

    commit

--- srcs/change_sections_offsets.c
+++ srcs/change_sections_offsets.c
@@ -21,23 +21,13 @@ size_t get_section_offset(int index, struct ELFheaders64 file_header) {
 
 int get_section_index(char *section, char *buf, struct sheaders64 *sheaders, struct ELFheaders64 file_header) {
     int i = 0, index = -1;
-    unsigned char section_found = 0;
     char *name = NULL;
     size_t offset = 0;
 
     while(i < file_header.e_shnum) {
         name = get_section_name_64(buf, file_header, sheaders, sheaders[i].sh_name);
-        printf("name = %s\n", name);
         if (name && strncmp(name, section, strlen(section)) == 0) {
             index = i;
-            section_found = 1;
-            offset = get_section_offset(i, file_header);
-            memcpy(&(buf[offset + SIZE]), &(sheaders[i].sh_size) + VIRUS_SIZE, 8);
-            ft_print_hexa_raw(&(buf[offset + SIZE]), 8);
-        }
-        if (section_found) {
-            offset = get_section_offset(i, file_header);
-            buf[offset + OFFSET] = sheaders[i].sh_offset + 255;
         }
         free(name);
         i++;
@@ -47,14 +37,13 @@ int get_section_index(char *section, char *buf, struct sheaders64 *sheaders, str
 
 int change_offset_sections(char *buf, struct sheaders64 *sheaders, struct ELFheaders64 file_header) {
     struct sheaders64 *sheaders2;
-    int index = 0;
+    size_t index = 0;
 
-    index = get_section_index(".text", buf, sheaders, file_header);
+    index = get_section_index("text", buf, sheaders, file_header);
     if (index == -1) {
         dprintf(1, "Error: section non trouvee\n");
-        return (-1);
     }
-    dprintf(1, "index = %d\n", index);
+    dprintf(1, "index = %ld\n", index);
     sheaders2 = get_section_headers_64(file_header, buf);
     print_sHeader64(sheaders2[index]);
     print_sHeader64(sheaders[index]);
--- srcs/write_file.c
+++ srcs/write_file.c
@@ -1,5 +1,32 @@
 #include "../include/woody.h"
 
+int get_section_index(char *section, char *buf, struct sheaders64 *sheaders, struct ELFheaders64 file_header) {
+    int i = 0, index = -1;
+    unsigned char section_found = 0;
+    char *name = NULL;
+    size_t offset = 0;
+
+    while(i < file_header.e_shnum) {
+        name = get_section_name_64(buf, file_header, sheaders, sheaders[i].sh_name);
+        printf("name = %s\n", name);
+        if (name && strncmp(name, section, strlen(section)) == 0) {
+            index = i;
+            section_found = 1;
+            offset = get_section_offset(i, file_header);
+            memcpy(&(buf[offset + SIZE]), &(sheaders[i].sh_size) + VIRUS_SIZE, 8);
+            ft_print_hexa_raw(&(buf[offset + SIZE]), 8);
+        }
+        if (section_found) {
+            offset = get_section_offset(i, file_header);
+            memcpy(&(buf[offset + OFFSET]), &(sheaders[i].sh_offset) + VIRUS_SIZE, 8);
+            ft_print_hexa_raw(&(buf[offset + OFFSET]), 8);
+        }
+        free(name);
+        i++;
+    }
+    return (index);
+}
+
 int write_in_file(char *data, size_t size) {
     int fd = open("woody", O_WRONLY | O_CREAT, S_IRWXU);
 
